5be63e7d7e0efe247e33abcdb3f327d6d4520747
unknown <litadubrova@gmail.com>
2019-07-23 19:52:19 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            for (int i = 1; i <= 21; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    PrintValue(i, "FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    PrintValue(i, "Buzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    PrintValue(i, "Fizz");
+                }
+                else
+                {
+                    PrintValue(i);
+                }
+
+            }
+
+        }
+        static void PrintValue(int i, string text)
+        {
+            Console.WriteLine($"{i} {text}");
+        }
+    }
+}
